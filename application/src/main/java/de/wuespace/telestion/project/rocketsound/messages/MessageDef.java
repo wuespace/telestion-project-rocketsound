f5331f3a6f4ea1dc1dc06a0b96e7d5e61dff90d2
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-08-05 02:53:43 +0200

    fix(application): Fix invalid/outdated usage of `DBResponse` in packages sending messages on the event bus

--- application/src/main/java/de/wuespace/telestion/project/rocketsound/messages/MessageDef.java
+++ application/src/main/java/de/wuespace/telestion/project/rocketsound/messages/MessageDef.java
@@ -2,33 +2,33 @@ package de.wuespace.telestion.project.rocketsound.messages;
 
 import de.wuespace.telestion.project.rocketsound.messages.base.*;
 import de.wuespace.telestion.project.rocketsound.messages.sound.*;
-import de.wuespace.telestion.services.database.DbResponse;
+import de.wuespace.telestion.extension.mongodb.DbResponse;
 
 import java.util.List;
 
 public class MessageDef {
 	public static void main(String[] args) {
 		System.out.println("org.telestion.core.database.MongoDatabaseService/out#save/de.jvpichowski.rocketsound.messages.sound.Amplitude");
-		System.out.println(new DbResponse(Amplitude.class, List.of(new Amplitude().json())).json());
+		System.out.println(new DbResponse(List.of(new Amplitude().json())).json());
 		System.out.println("org.telestion.core.database.MongoDatabaseService/out#save/de.jvpichowski.rocketsound.messages.sound.Spectrum");
-		System.out.println(new DbResponse(Spectrum.class, List.of(new Spectrum().json())).json());
+		System.out.println(new DbResponse(List.of(new Spectrum().json())).json());
 		System.out.println("org.telestion.core.database.MongoDatabaseService/out#save/de.jvpichowski.rocketsound.messages.base.Velocity");
-		System.out.println(new DbResponse(Velocity.class, List.of(new Velocity().json())).json());
+		System.out.println(new DbResponse(List.of(new Velocity().json())).json());
 		System.out.println("org.telestion.core.database.MongoDatabaseService/out#save/de.jvpichowski.rocketsound.messages.base.NineDofData");
-		System.out.println(new DbResponse(NineDofData.class, List.of(new NineDofData(
+		System.out.println(new DbResponse(List.of(new NineDofData(
 				new Accelerometer(),
 				new Gyroscope(),
 				new Magnetometer()
 		).json())).json());
 		System.out.println("org.telestion.core.database.MongoDatabaseService/out#save/de.jvpichowski.rocketsound.messages.base.BaroData");
-		System.out.println(new DbResponse(BaroData.class, List.of(new BaroData(
+		System.out.println(new DbResponse(List.of(new BaroData(
 				new Pressure(),
 				new Temperature(),
 				new Altitude()
 		).json())).json());
 		System.out.println("org.telestion.core.database.MongoDatabaseService/out#save/de.jvpichowski.rocketsound.messages.base.GpsData");
-		System.out.println(new DbResponse(GpsData.class, List.of(new GpsData().json())).json());
+		System.out.println(new DbResponse(List.of(new GpsData().json())).json());
 		System.out.println("org.telestion.core.database.MongoDatabaseService/out#save/de.jvpichowski.rocketsound.messages.base.FlightState");
-		System.out.println(new DbResponse(FlightState.class, List.of(new FlightState().json())).json());
+		System.out.println(new DbResponse(List.of(new FlightState().json())).json());
 	}
 }
